5501e9995a3094d30e2f936a7a3ec4439a4457b3
Brandon Kim <brandonkim@Brandons-Air-86.lan>
2024-09-23 11:24:37 -0700

    updated variable names and comments

--- fib.c
+++ fib.c
@@ -4,64 +4,54 @@
 
 // Recursive fib(). Since fibonacci #1 is 0, adjust the other values
 // to fit the constraints
-unsigned long long int rfib(unsigned long long int n) {
-   if (n <= 1) {
+unsigned long long rfib(unsigned long long num) {
+   if (num <= 1) {
       return 0;
-   } else if (n == 2) {
+   } else if (num == 2) {
       return 1;
    }
 
-   // Using recursion, find fib(n)
-   return rfib(n - 1) + rfib(n - 2);
+   return rfib(num - 1) + rfib(num - 2);
 }                                   
 
 // Iterative fib(). Same as above, make sure fib(1) is 0. Then adjust
 // the other values
-unsigned long long int ifib(unsigned long long int n) { 
-   if (n == 1) {
+unsigned long long ifib(unsigned long long num) { 
+   if (num <= 1) {
       return 0;
-   } else if (n == 2) {
+   } else if (num == 2) {
       return 1;                       
    }
 
-   // Set the initial values for the iterative sequence
-   unsigned long long int a = 0;
-   unsigned long long int b = 1;
-   unsigned long long int result = 0;                    
+   unsigned long long previous = 0;
+   unsigned long long current = 1;
+   unsigned long long result = 0;                    
 
-   // Update the result and variables for the next iteration
-   for (int i = 3; i <= n; ++i) {
-      result = a + b;
-      a = b;
-      b = result;
+   for (int ix = 3; ix <= num; ++ix) {
+      result = previous + current;
+      previous = current;
+      current = result;
    }                                  
 
-   // Return the result.
    return result;
 }                                     
 
 int main(int argc, char* argv[]) {
-   // Create and store the cmd line integer into a variable
-   unsigned long long int n;
-   sscanf(argv[1], "%llu", &n);
+   unsigned long long n_input;
+   sscanf(argv[1], "%llu", &n_input);
 
-   // Store the type of method
-   char type = argv[2][0];            
+   char method_type = argv[2][0];
 
-   // If the type of method is iterative, call ifib(),
-   // otherwise call rfib(). If the type is invalid,
-   // set the result to 0.
-   unsigned long long int number = 0;
-   if (type == 'i') {
-      number = ifib(n);
-   } else if (type == 'r') {
-      number = rfib(n);
+   unsigned long long result = 0;
+   if (method_type == 'i') {
+      result = ifib(n_input);
+   } else if (method_type == 'r') {
+      result = rfib(n_input);
    } else {
-      number = 0;
+      result = 0;
    }
 
-   // Print the result
-   printf("%llu", number);
+   printf("%llu", result);
 
    return 0;
 }
